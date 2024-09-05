564f3194315821a5e36e80ad3edbd4cc83862e38
Cody Keats <cocopuff2u@yahoo.com>
2024-09-05 12:42:48 -0400

    settting up rules

--- SecuraMac.zsh
+++ SecuraMac.zsh
@@ -18,6 +18,9 @@
 #
 ####################################################################################################
 
+# Define the maximum number of allowed attempts for any input
+MAX_ATTEMPTS=3
+
 # Functions to set text attributes and colors using tput
 export RESET_FORMAT='tput sgr0'
 export BOLD='tput bold'
@@ -26,17 +29,17 @@ export REVERSE='tput rev'
 
 # Basic Colors
 export RED='tput setaf 1'
-export GREEN='tput setaf 2'
+export GREEN='tput setaf 10'
 export BLUE='tput setaf 4'
 export YELLOW='tput setaf 3'
 export MAGENTA='tput setaf 5'
 export CYAN='tput setaf 6'
 export WHITE='tput setaf 7'
+export ORANGE='tput setaf 214'
 
 # Background Colors
 export RED_BG='tput setab 1'
 export GREEN_BG='tput setab 2'
-export BLUE_BG='tput setab 4'
 export DEFAULT_BG='tput setab 0'
 
 # Bright Colors
@@ -47,17 +50,25 @@ export BRIGHT_BLUE='tput setaf 12'
 # 256 Colors
 export COLOR256='tput setaf 82'
 
-
 # Functions to print text with various colors and attributes
 print_red() {
     eval "$RED"
     printf "%b%s%b\n" "$1" "$(eval "$RESET_FORMAT")"
 }
 
+print_orange() {
+    eval "$ORANGE"
+    printf "%b%s%b\n" "$1" "$(eval "$RESET_FORMAT")"
+}
+
 print_green() {
     eval "$GREEN"
+    printf "%b%s%b\n" "$1" "$(eval "$RESET_FORMAT")"
+}
+
+print_green_bold() {
+    eval "$GREEN"
     eval "$BOLD"
-    eval "$BLUE_BG"
     printf "%b%s%b\n" "$1" "$(eval "$RESET_FORMAT")"
 }
 
@@ -92,6 +103,12 @@ print_white() {
     printf "%b%s%b\n" "$1" "$(eval "$RESET_FORMAT")"
 }
 
+print_white_bold() {
+    eval "$WHITE"
+    eval "$BOLD"
+    printf "%b%s%b\n" "$1" "$(eval "$RESET_FORMAT")"
+}
+
 print_red_bg() {
     eval "$RED_BG"
     eval "$RED"
@@ -104,14 +121,14 @@ print_green_bg() {
     printf "%b%s%b\n" "$1" "$(eval "$RESET_FORMAT")"
 }
 
-print_blue_bg() {
-    eval "$BLUE_BG"
-    eval "$BLUE"
+print_bright_red() {
+    eval "$BRIGHT_RED"
     printf "%b%s%b\n" "$1" "$(eval "$RESET_FORMAT")"
 }
 
-print_bright_red() {
+print_bright_red_bold() {
     eval "$BRIGHT_RED"
+    eval "$BOLD"
     printf "%b%s%b\n" "$1" "$(eval "$RESET_FORMAT")"
 }
 
@@ -130,66 +147,203 @@ print_color256() {
     printf "%b%s%b\n" "$1" "$(eval "$RESET_FORMAT")"
 }
 
-eval "$BLUE_BG"
+# Rainbow color function
+print_rainbow() {
+    local text="$1"
+    local i=0
+    local colors=("$RED" "$YELLOW" "$GREEN" "$CYAN" "$WHITE" "$MAGENTA")
+    
+    for ((i=0; i<${#text}; i++)); do
+        eval "${colors[$((i % 6))]}"
+        printf "%s" "${text:$i:1}"
+    done
+    printf "\n"
+    eval "$RESET_FORMAT"
+}
 
-# Clear the screen to apply the background color
+# Clear the screen
 clear
 
-# ASCII Logo with SecuraMac 
-print_magenta_bold "                                                      "
-print_magenta_bold "   _____                           __  __             "
-print_magenta_bold "  / ____|                         |  \/  |            "
-print_magenta_bold " | (___   ___  ___ _   _ _ __ __ _| \  / | __ _  ___  "
-print_magenta_bold "  \___ \ / _ \/ __| | | | '__/ _' | |\/| |/ _' |/ __| "
-print_magenta_bold "  ____) |  __/ (__| |_| | | | (_| | |  | | (_| | (__  "
-print_magenta_bold " |_____/ \___|\___|\__,_|_|  \__,_|_|  |_|\__,_|\___| "
-print_magenta_bold "                                                      "
-print_magenta "https://github.com/cocopuff2u/SecuraMac               "
-print_magenta ""
-print_green "SecuraMac simplifies macOS security, offering quick and effective tools to fortify your system and ensure protection. "
-print_magenta ""
-print_magenta ""
+intro_logo() {
+    # ASCII Logo with SecuraMac 
+    print_green_bold "                                                                                "
+    print_green_bold "┌──────────────────────────────────────────────────────────────────────────────┐"
+    print_green_bold "│                                                                              │"
+    print_green_bold "│ ███████╗███████╗ ██████╗██╗   ██╗██████╗  █████╗ ███╗   ███╗ █████╗  ██████╗ │"
+    print_green_bold "│ ██╔════╝██╔════╝██╔════╝██║   ██║██╔══██╗██╔══██╗████╗ ████║██╔══██╗██╔════╝ │"
+    print_green_bold "│ ███████╗█████╗  ██║     ██║   ██║██████╔╝███████║██╔████╔██║███████║██║      │"
+    print_green_bold "│ ╚════██║██╔══╝  ██║     ██║   ██║██╔══██╗██╔══██║██║╚██╔╝██║██╔══██║██║      │"
+    print_green_bold "│ ███████║███████╗╚██████╗╚██████╔╝██║  ██║██║  ██║██║ ╚═╝ ██║██║  ██║╚██████╗ │"
+    print_green_bold "│ ╚══════╝╚══════╝ ╚═════╝ ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝     ╚═╝╚═╝  ╚═╝ ╚═════╝ │"
+    print_green_bold "│ https://github.com/cocopuff2u/SecuraMac                                      │"
+    print_green_bold "└──────────────────────────────────────────────────────────────────────────────┘"
+    print_green_bold ""
 
-# Prompt for user options
-print_green "Welcome to SecuraMac!"
-print_green ""
-print_green "Please select an option:"
-print_green "1. Perform Full SecuraMac"
-print_green "2. Perform Log SecuraMac"
-print_green "3. Perform Setting SecuraMac"
-print_green "4. Full System Report"
-print_green "5. Exit"
+    # Prompt for user options
+    print_white_bold "Welcome to SecuraMac!"
+    print_white ""
+    print_white "SecuraMac simplifies macOS security, offering quick and effective tools to fortify your system and ensure protection. "
+    print_white ""
+}
 
-# Print prompt in yellow and read input on the same line
-printf "%bEnter your choice [1-5]: %b" "$(eval "$YELLOW")" "$(eval "$RESET_FORMAT")"; read choice
+intro_logo
 
+print_orange "##############################" 
+print_orange "# BEFORE PROCEEDING"
+print_orange "##############################"  
+print_orange ""
+print_orange "      1. Maximize Terminal Window to Full Screen or Largest Possible Size"
+print_orange "      2. Verify terminal has full disk access"
+print_orange "      3. Data loss is possible; ensure you have a backup"
+print_orange ""
+print_orange ""
+print_orange "I have confirmed and i want to proceed:"
+print_orange "1. Yes"
+print_orange "2. No" 
+print_orange "" 
 
-# Handle user input
-case $choice in
-  1)
-    print_green "You selected Task 1"
-    # Insert code to perform Task 1
-    ;;
-  2)
-    print_green "You selected Task 2"
-    # Insert code to perform Task 2
-    ;;
-  3)
-    print_green "You selected Task 3"
-    # Insert code to perform Task 3
-    ;;
-  4)
-    print_green "You selected Task 4"
-    # Insert code to perform Task 4
-    ;;
-  5)
-    print_green "Exiting..."
-    # Reset background color to default before exiting
-    clear
-    echo "SecuraMac Exited"
+# Loop until a valid choice is made
+while true; do
+    printf "%bEnter your choice [Y/N]: %b" "$(eval "$ORANGE")" "$(eval "$RESET_FORMAT")"; read choice
+
+    # Process user input
+    case "$choice" in
+        1|y|Y|yes|YES)
+            print_orange "Proceeding..."
+            sleep 1
+            clear
+            print_green "Verifying Full Disk Access...."
+            sleep 1
+            clear
+            # Place the code to execute if the user chooses Yes here
+            break
+            ;;
+        2|n|N|no|NO)
+            print_orange "Operation aborted."
+            exit 1
+            ;;
+        *)
+            print_bright_red_bold "Invalid choice. Please enter y for Yes or n for No."
+            ;;
+    esac
+done
+
+# Attempting to check Full Disk Access for Terminal
+if sudo test -r "/Library/Application Support/com.apple.TCC/TCC.db" 2>/dev/null; then
+    intro_logo
+else
+    print_bright_red_bold "Terminal does not have Full Disk Access."
+    print_bright_red_bold "Please grant Full Disk Access to Terminal by following these steps:"
+    print_bright_red_bold "1. Open System Settings."
+    print_bright_red_bold "2. Go to Privacy & Security > Full Disk Access."
+    print_bright_red_bold "3. Click the '+' button and add Terminal from /Applications/Utilities."
+    print_bright_red_bold "4. If prompted, enter your password to make changes."
+    print_bright_red_bold "5. Restart Terminal for the changes to take effect."
     exit 0
-    ;;
-  *)
-    print_white "Invalid option. Please select a valid option (1-5)."
-    ;;
-esac
+fi
+
+print_green "##############################" 
+print_green "# Please select an option:"
+print_green "##############################" 
+print_green ""
+print_green "1. Perform Full SecuraMac (Runs Full Security Options)"
+print_green "2. Perform Log SecuraMac (Runs Only Logging Security Options)"
+print_green "3. Perform Disable SecuraMac (Gives Options to Disable)"
+print_green "4. Full SecuraMac System Report (Shows report of current settings)"
+print_green "5. Exit"
+print_green ""
+
+while true; do
+    # Print prompt in yellow and read input on the same line
+    printf "%bEnter your choice [1-5]: %b" "$(eval "$GREEN")" "$(eval "$RESET_FORMAT")"; read choice
+
+    # Handle user input
+    case "$choice" in
+    1)
+        print_green "You selected Peform Full SecuraMac"
+        print_green ""
+        break
+        # Insert code to perform Task 1
+        ;;
+    2)
+        print_green "You selected Perform Log SecuraMac"
+        print_green ""
+        break
+        # Insert code to perform Task 2
+        ;;
+    3)
+        print_green "You selected Perform Disable SecuraMac"
+        print_green ""
+        break
+        # Insert code to perform Task 3
+        ;;
+    4)
+        print_green "You selected Full Secura System Report"
+        print_green ""
+        break
+        # Insert code to perform Task 4
+        ;;
+    5)
+        print_green "You selected exit SecuraMac..."
+        sleep 1
+        clear
+        echo "SecuraMac Closed"
+        exit 0
+        ;;
+    *)
+        print_bright_red_bold "invalid choice. Please select a valid option (1-5)."
+        ;;
+    esac
+done
+
+
+# Just works
+Rule_Name="Firewall"
+Rule_Recommend="Enable Firewall?"
+Rule_Description="This helps protect your Mac from being attacked over the internet"
+Rule_Check='sudo /usr/libexec/ApplicationFirewall/socketfilterfw --getglobalstate | grep -q "enabled" && echo "true" || echo "false"'
+Rule_Enable="sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setglobalstate on"
+Rule_Disable="sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setglobalstate off"
+
+Rule_Name="Stealth Mode"
+Rule_Recommend="Enable Stealth Mode?"
+Rule_Description="Your Mac will not respond to ping requests or connection attempts from TCP and UDP networks"
+Rule_Check='sudo /usr/libexec/ApplicationFirewall/socketfilterfw --getstealthmode | grep -q "enabled" && echo "true" || echo "false"'
+Rule_Enable="sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setstealthmode on"
+Rule_Disable="sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setstealthmode off"
+
+# User Safety Considerations 
+Rule_Name="Printer Sharing"
+Rule_Recommend="Disable Printer Sharing?"
+Rule_Description="Offers redundancy in case the Firewall was not configured"
+Rule_Check='sudo cupsctl | grep 'SharePrinters' | grep -q "Yes" && echo "true" || echo "false"'
+Rule_Enable="sudo cupsctl --share-printers"
+Rule_Disable="sudo cupsctl --no-share-printers"
+
+
+# Requires Full Disk access
+Rule_Name="SSH"
+Rule_Recommend="Do you use SSH?"
+Rule_Description="If no, remote login will be turned off"
+Rule_Check='sudo systemsetup -getremotelogin | grep -q "on" && echo "true" || echo "false"'
+Rule_Enable="sudo systemsetup -f -setremotelogin on"
+Rule_Disable="sudo systemsetup -f -setremotelogin off"
+
+
+
+# Only enable via manual
+Rule_Name="GateKeepeer"
+Rule_Recommend="Enable GateKeeper?"
+Rule_Description="Defend against malware by enforcing code signing and verifying downloaded applications before letting them to run"
+Rule_Check='sudo spctl --status'
+
+# CANT CONFIRM THESE BELO
+Rule_Name="Firewall Logging"
+Rule_Check='sudo /usr/libexec/ApplicationFirewall/socketfilterfw --getloggingmode | grep -q "on" && echo "true" || echo "false"'
+Rule_Enable="sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setloggingmode on"
+Rule_Disable="sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setloggingmode off"
+
+Rule_Name="Firewall Logging Detail Level"
+Rule_Check='sudo /usr/libexec/ApplicationFirewall/socketfilterfw --getloggingmode | grep -q "on" && echo "true" || echo "false"'
+Rule_Enable="sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setloggingmode on"
+Rule_Disable="sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setloggingmode off"
--- SecuraMac2.zsh
+++ SecuraMac2.zsh
@@ -1,24 +1,35 @@
+if ! plutil -lint /Library/Preferences/com.apple.SoftwareUpdate.plist >/dev/null 2>&1; then
+    print_bright_red_bold "Terminal does not have Full Disk Access."
+    print_bright_red_bold "Please grant Full Disk Access to Terminal by following these steps:"
+    print_bright_red_bold "1. Open System Preferences."
+    print_bright_red_bold "2. Go to Privacy & Security > Full Disk Access."
+    print_bright_red_bold "3. Click the lock icon to make changes (you may need to enter your password)."
+    print_bright_red_bold "4. Click the '+' button and add Terminal from /Applications/Utilities."
+    print_bright_red_bold "5. Restart Terminal for the changes to take effect."
+    exit 0
+fi
+
 #!/bin/zsh
 
-# Define color variables
-BLUE_BG='\033[44m'
-RED='\033[31m'
-RESET='\033[0m'
+# Define the file to check
+FILE="/Library/Application Support/com.apple.TCC/TCC.db"
 
-printf "%b" "${BLUE_BG}"
+# Attempt to check read access to the file and capture output and errors
+OUTPUT=$(sudo test -r "$FILE" 2>&1)
+EXIT_STATUS=$?
 
-clear
-
-print_magenta() {
-    printf "%b%s%b\n" "${BLUE_BG}${MAGENTA}" "$1" "${RESET}"
-}
-
-# Debug by printing each color variable
-print_magenta "testing things"
-printf "Blue BG: ${BLUE_BG}Text${RESET}\n"
-printf "Red: ${RED}Text${RESET}\n"
-
-# Print text with background and foreground color using variables
-printf "${BLUE_BG}${RED}Testing${RESET}\n"
-
-print_magenta "testing this thing"
+if [[ $EXIT_STATUS -ne 0 ]]; then
+  if [[ $EXIT_STATUS -eq 1 ]]; then
+    # Check if it's a permission error
+    echo "This script requires Full Disk Access."
+    echo "Add Terminal to the Full Disk Access list in System Preferences > Privacy & Security."
+    echo "Quit Terminal and re-run this script after granting Full Disk Access."
+  else
+    # Other errors
+    echo "Error accessing the file: $FILE"
+    echo "Error details: $OUTPUT"
+  fi
+else
+  # File accessed successfully
+  echo "Terminal has Full Disk Access."
+fi
